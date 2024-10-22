e2197d46c1640ed05a83e69d518a60aaedbd648e
AaNn05 <117485357+AaNn05@users.noreply.github.com>
2024-10-22 16:45:50 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Sort;
+
+int[] arr = { 65, 35, 26, 13, 13, 12, 91 };
+
+SortingAlgorithm.PrintArray(arr);
+SortingAlgorithm.ShellSort(arr);
+SortingAlgorithm.PrintArray(arr);
---
+++ SellSort.cs
@@ -0,0 +1,36 @@
+﻿namespace Sort
+{
+    public static class SortingAlgorithm
+    {
+        public static int ShellSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int g = n / 2; g > 0; g /= 2)
+            {
+                for (int i = g; i < n; i += 1)
+                {
+                    int temp = arr[i];
+
+                    int j;
+                    for (j = i; j >= g && arr[j - g] > temp; j -= g)
+                        arr[j] = arr[j - g];
+
+                    arr[j] = temp;
+                }
+            }
+            return 0;
+        }
+
+        public static int PrintArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            return 0;
+        }
+    }
+}
